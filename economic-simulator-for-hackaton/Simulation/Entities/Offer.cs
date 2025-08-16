d3bb866032a13debac8d336b2bfcb081d548d9d5
geniuslat <traber73@yandex.ru>
2025-08-17 01:21:41 +0500

    UpdatePrice

--- economic-simulator-for-hackaton/Simulation/Entities/Offer.cs
+++ economic-simulator-for-hackaton/Simulation/Entities/Offer.cs
@@ -16,7 +16,10 @@ public class Offer
 
     public uint WasUsedYesterday { get; set; }
 
-    public uint? Border { get; set; } = 1;
+    public uint? QuantityBorder { get; set; } = 1;
+
+    //Will not sell cheaper, will not buy more expensive
+    public float PriceBorder { get; set; }
 
     public override string ToString()
     {
@@ -26,9 +29,9 @@ public class Offer
             typeStr = "скупает";
         }
         var BorderStr = "-";
-        if (Border is not null)
+        if (QuantityBorder is not null)
         {
-            BorderStr = Border.ToString();
+            BorderStr = QuantityBorder.ToString();
         }
         ///////$"Наименование товара \t вид предложения \t цена за штуку \t верхний предел товара \t Автор предложения"; 
         return $"\t{Item.TypeToString(ItemType)}|\t {typeStr}|\t {pricePerOne}|\t { BorderStr}|\t {Offerer.Name}";
@@ -42,9 +45,9 @@ public class Offer
             typeStr = "скупает";
         }
         var BorderStr = "-";
-        if (Border is not null)
+        if (QuantityBorder is not null)
         {
-            BorderStr = Border.ToString();
+            BorderStr = QuantityBorder.ToString();
         }
         ///////$"Наименование товара \t вид предложения \t цена за штуку \t верхний предел товара \t Автор предложения"; 
         return new List<string>()
@@ -57,5 +60,58 @@ public class Offer
             Offerer.Name,
 
         };
+    }
+
+    public float UpdatePrice()
+    {
+        if (IsOffererSelling)
+        {
+            if(WasUsedYesterday == 0)
+            {
+                pricePerOne -= pricePerOne * 0.1f;
+                if (pricePerOne < PriceBorder)
+                {
+                    pricePerOne = PriceBorder;
+                }
+                WasUsedYesterday = 0;
+                return pricePerOne;
+            }
+            else
+            {
+                //float salesRatio = (float)WasUsedYesterday / QuantityBorder;
+                //if (salesRatio > 1f) salesRatio = 1f;
+                //float maxIncrease = 0.15f;
+                //float increaseRate = maxIncrease * salesRatio;
+
+                var increaseRate = 0.1f;
+                pricePerOne += pricePerOne * increaseRate;
+                WasUsedYesterday = 0;
+                return pricePerOne;
+            }
+        }
+        else
+        {
+            if(Offerer.moneyBalance < PriceBorder)
+            {
+                PriceBorder = Offerer.moneyBalance;
+            }
+
+            if (WasUsedYesterday == 0)
+            {
+                pricePerOne += pricePerOne * 0.1f;
+                if (pricePerOne > PriceBorder)
+                {
+                    pricePerOne = PriceBorder;
+                }
+                WasUsedYesterday = 0;
+                return pricePerOne;
+            }
+            else
+            {
+                pricePerOne -= pricePerOne * 0.1f;
+                WasUsedYesterday = 0;
+                return pricePerOne;
+            }
+        }
     }
 }
